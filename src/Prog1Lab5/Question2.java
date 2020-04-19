9b6afcc5acc67fe1d5fb53ee337f84416f0b5c76
linaboshra <60351601+linaboshra@users.noreply.github.com>
2020-04-19 18:41:31 -0400

    Question 2 added

--- src/Prog1Lab5/Question2.java
+++ src/Prog1Lab5/Question2.java
@@ -16,6 +16,8 @@
  */
 package Prog1Lab5;
 
+import java.util.Scanner;
+
 /**
  * This program computes the greatest common divisor of two integers.
  * 
@@ -30,7 +32,34 @@ public class Question2 {
     
     public static void main(String[] args){
         
+        Scanner input = new Scanner(System.in);
         
+        int gcd = 0;
+        
+        System.out.println("Enter an integer");
+        
+        while (!input.hasNextInt()){
+            System.out.println("Invalid input, please enter an integer");
+            input.nextLine();
+        }
+        
+        int int1 = input.nextInt();
+        
+        System.out.println("Enter another integer");
+        
+        while (!input.hasNextInt()){
+            System.out.println("Invalid input, please enter an integer");
+            input.nextLine();
+        }
+        
+        int int2 = input.nextInt();
+        
+        for (int i = 1; i <= int1 && i <= int2; i++) { //Goes through all the numbers between int1 and int2
+            if(int1%i==0 && int2%i==0){ //Any number that is a divisor of these two numbers gets assigened to gcd
+               gcd = i; //At the end of the iteration, the last number that gets assigend to gcd is the greatest common divisor
+            }   
+        }
+        
+        System.err.println("The greatest common divisor is " + gcd);
     }
-    
 }
