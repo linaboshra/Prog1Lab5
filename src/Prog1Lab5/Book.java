4901adeaae2d9b69c74759870c75781902ccd19e
linaboshra <60351601+linaboshra@users.noreply.github.com>
2020-04-21 22:21:14 -0400

    Added Book Class

---
+++ src/Prog1Lab5/Book.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2020 Lina Boshra
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package Prog1Lab5;
+
+/**
+ * This class encapsulates the concept of a book.
+ * 
+ * @author Lina Boshra
+ */
+
+public class Book {
+    
+    private String title;
+    private String author;
+    private String ISBN;
+    private String yearOfPublishing;
+    private static String publisher = "Vanier";
+    private int amountOfBooks = 0;
+    
+    public Book(String bookTitle, String bookAuthor, String bookISBN, String bookYearOfPublishing){
+        title = bookTitle;
+        author = bookAuthor;
+        ISBN = bookISBN;
+        yearOfPublishing = bookYearOfPublishing;
+    }
+    
+    public String getTitle(){
+        return title;
+    }
+    
+    public String getAuthor(){
+        return author;
+    }
+    
+    public String getISBN(){
+        return ISBN;
+    }
+    
+    public String getYearOfPublishing(){
+        return yearOfPublishing;
+    }
+    
+    public String getPublisher(){
+        return publisher;
+    }
+    
+    public int getAmountOfBooks(){
+        return amountOfBooks;
+    }
+    
+    public void printTitle(){
+        System.out.println("Title: " + title);
+    }
+    
+    public void printAuthor(){
+        System.out.println("Author: " + author);
+    }
+    
+    public void printISBN(){
+        System.out.println("ISBN: " + ISBN);
+    }
+    
+    public void printYearOfPublishing(){
+        System.out.println("Year of publishing: " + yearOfPublishing);
+    }
+    
+    public void printPublisher(){
+        System.out.println("Publisher: " + publisher);
+    }
+    
+    void addBook(){
+        amountOfBooks ++;
+    }
+}
